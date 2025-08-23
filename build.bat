5120b47d14096ece58f96a0dc12b697e71c84724
omicronrex <65692652+omicronrex@users.noreply.github.com>
2025-08-23 11:19:17 -0300

    move to subfolder

--- build.bat
+++ build.bat
@@ -1,11 +1,6 @@
 @echo off
 
-del gm82audio.dll
-
 cmake -A Win32 -B build && cmake --build build --config Release
-
-move build\Release\gm82audio.dll gm82audio.dll
-
 py gm82gex.py gm82audio.gej
 
 pause
